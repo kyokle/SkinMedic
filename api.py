a8e393d39ff788bc8c6b80dda1dca75de58f95f1
kyokle <jlouise1425@gmail.com>
2026-04-25 00:35:29 +0800

    add Dockerfile for HuggingFace

--- api.py
+++ api.py
@@ -247,5 +247,5 @@ def analyze():
 
 if __name__ == "__main__":
     import os
-    print("✅ SkinMedic API running on http://127.0.0.1:8002")
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8002)), debug=False)
+    print("✅ SkinMedic API running on http://127.0.0.1:7860")
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 7860)), debug=False)
