b867a2e969b1f02c4a035c18f2982f85a18f720c
kyokle <jlouise1425@gmail.com>
2026-05-04 16:11:50 +0800

    fix doctor bookings sidebar pfp

--- app/Http/Controllers/DoctorBookingsController.php
+++ app/Http/Controllers/DoctorBookingsController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\SidebarDoctorController;
+use App\Http\Controllers\SidebarDataController;
 use App\Helpers\NotificationHelper;
 
 class DoctorBookingsController extends Controller
 {
+    use SidebarDataController;
     public function index(Request $request)
     {
         $doctorId     = session('user_id');
